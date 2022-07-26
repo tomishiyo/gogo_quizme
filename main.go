72856ab83ca48a0d452fe5da3a286175f92af363
tomishiyo <tomishiyo@gmail.com>
2022-07-26 02:34:09 -0300

    Added function ruz_quiz and function print_score.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-// Gets the quiz file to be used
+// Process the quiz file to be used
 // Defaults to problems.csv
 func set_quiz_file() string {
 	if len(os.Args) == 1 {
@@ -18,9 +18,7 @@ func set_quiz_file() string {
 		return os.Args[1]
 	} else {
 		error_msg := "Usage: " + os.Args[0] + " <quizz_name>"
-		fmt.Println(error_msg)
-		os.Exit(1)
-		return ""
+		panic(error_msg)
 	}
 
 }
@@ -29,6 +27,49 @@ func check_error(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+// Runs the quiz: loop over all questions prompting the user for an answer
+func run_quiz(questions [][]string) (score int) {
+	var user_answer, answer, question_text string
+
+	fmt.Println("")
+	fmt.Println(">>>>>>>>Welcome to GOGO_QUIZME!<<<<<<<<<<<<")
+	fmt.Println("")
+	fmt.Println("Rule N.1 : All numerical answers must be numbers!")
+	fmt.Println("For example, 1+1 must be answered as 2 not as Two")
+	fmt.Println("")
+	fmt.Println("Rule N.2: Ordinal numbers must be writen in abbreviated form!")
+	fmt.Println("For example, Neil Armstrong was the 1st man in the moon (as opossed to 'first')")
+	fmt.Println("")
+	fmt.Println("Have fun!")
+
+	for i, question := range questions {
+		question_text = "Question " + fmt.Sprint(i+1) + ": " + question[0]
+		fmt.Println(question_text)
+		// Print without the \n allows for the formatted input
+		fmt.Print(">>")
+		// The Scanln does not capture the \n input
+		fmt.Scanln(&user_answer)
+		// strings.ToLower remove caps problems
+		user_answer = strings.ToLower(user_answer)
+
+		answer = strings.ToLower(question[1])
+		if answer == user_answer {
+			score += 1
+		}
+	}
+
+	return score
+}
+
+func print_score(score, number_of_questions int) {
+	fmt.Println("And DONE ...")
+	fmt.Printf("In total, %v questions were answered!\n", number_of_questions)
+	fmt.Println("")
+	fmt.Println("#######################################")
+	fmt.Printf("              You scored %v!          \n", score)
+	fmt.Println("#######################################")
 }
 
 func main() {
@@ -46,7 +87,9 @@ func main() {
 	*/
 	questions, err := r.ReadAll()
 	check_error(err)
+	number_of_questions := len(questions)
 
-	fmt.Println(questions[0][0])
+	score := run_quiz(questions)
+	print_score(score, number_of_questions)
 
 }
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@ https://courses.calhoun.io/
 
 ## Exercise#1: Quiz Game
 
-Part 1
+### Part 1 ###
 Create a program that will read in a quiz provided via a CSV file (more details below) and will then give the quiz to a user keeping track of how many questions they get right and how many they get incorrect. Regardless of whether the answer is correct or wrong the next question should be asked immediately afterwards.
 
 The CSV file should default to problems.csv (example shown below), but the user should be able to customize the filename via a flag.
@@ -15,3 +15,12 @@ You can assume that quizzes will be relatively short (< 100 questions) and will 
 At the end of the quiz the program should output the total number of questions correct and how many questions there were in total. Questions given invalid answers are considered incorrect.
 
 NOTE: CSV files may have questions with commas in them. Eg: "what 2+2, sir?",4 is a valid row in a CSV. I suggest you look into the CSV package in Go and don’t try to write your own CSV parser.
+
+### Part 2 ###
+Adapt your program from part 1 to add a timer. The default time limit should be 30 seconds, but should also be customizable via a flag.
+
+Your quiz should stop as soon as the time limit has exceeded. That is, you shouldn’t wait for the user to answer one final questions but should ideally stop the quiz entirely even if you are currently waiting on an answer from the end user.
+
+Users should be asked to press enter (or some other key) before the timer starts, and then the questions should be printed out to the screen one at a time until the user provides an answer. Regardless of whether the answer is correct or wrong the next question should be asked.
+
+At the end of the quiz the program should still output the total number of questions correct and how many questions there were in total. Questions given invalid answers or unanswered are considered incorrect.
